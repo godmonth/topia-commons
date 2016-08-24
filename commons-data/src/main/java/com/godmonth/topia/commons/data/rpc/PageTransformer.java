68dc3d39d52ca55299847decface129064803f5c
godmonth <link2ibm@126.com>
2016-08-24 13:37:14 +0800

    c

--- commons-data/src/main/java/com/godmonth/topia/commons/data/rpc/PageTransformer.java
+++ commons-data/src/main/java/com/godmonth/topia/commons/data/rpc/PageTransformer.java
@@ -1,27 +1,22 @@
 package com.godmonth.topia.commons.data.rpc;
 
-import java.util.ArrayList;
-
 import org.springframework.data.domain.Page;
 
-import com.google.common.base.Function;
-import com.google.common.collect.Lists;
 import com.godmonth.topia.commons.pagination.Pagination;
 import com.godmonth.topia.commons.pagination.SortPageParam;
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 
 public class PageTransformer {
 
 	public static <IN, OUT> Pagination<OUT> transform(Page<IN> page, Function<IN, OUT> function) {
-		Pagination<OUT> pagination = new Pagination<OUT>();
-		if (page != null) {
-			pagination.setTotalElements(page.getTotalElements());
-			pagination.setTotalPages(page.getTotalPages());
-			pagination.setItems(Lists.transform(page.getContent(), function));
-		} else {
-			pagination.setTotalElements(0);
-			pagination.setTotalPages(0);
-			pagination.setItems(new ArrayList<OUT>());
+		if (page == null) {
+			return null;
 		}
+		Pagination<OUT> pagination = new Pagination<OUT>();
+		pagination.setTotalElements(page.getTotalElements());
+		pagination.setTotalPages(page.getTotalPages());
+		pagination.setItems(Lists.transform(page.getContent(), function));
 		pagination.setSortPageParam(new SortPageParam(page.getNumber(), page.getSize()));
 		return pagination;
 	}
