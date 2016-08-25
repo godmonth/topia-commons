a594427f554935f847e6d332057318cbe48e6624
godmonth <link2ibm@126.com>
2016-08-25 14:33:41 +0800

    c

--- commons-basic/src/main/java/com/godmonth/topia/commons/basic/exception/CustomeValidate.java
+++ commons-basic/src/main/java/com/godmonth/topia/commons/basic/exception/CustomeValidate.java
@@ -1,18 +1,26 @@
 package com.godmonth.topia.commons.basic.exception;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 public class CustomeValidate {
 	private CustomeValidate() {
 	}
 
 	public static void notNull(Object object, String errorCode, String message, final Object... values) {
 		if (object == null) {
-			throw new ErrorCodeException(errorCode, String.format(message, values));
+			if (ArrayUtils.isNotEmpty(values)) {
+				message = String.format(message, values);
+			}
+			throw new ErrorCodeException(errorCode, message);
 		}
 	}
 
 	public static void isTrue(boolean condition, String errorCode, String message, final Object... values) {
 		if (!condition) {
-			throw new ErrorCodeException(errorCode, String.format(message, values));
+			if (ArrayUtils.isNotEmpty(values)) {
+				message = String.format(message, values);
+			}
+			throw new ErrorCodeException(errorCode, message);
 		}
 	}
 }
